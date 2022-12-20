b14c2f29770cbbb8d7cfe636ff181fdab680f686
Mohammed samir <abutech8801@gmail.com>
2022-12-20 10:56:20 +0530

    auth complete

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("server....")
+const express = require("express")
+const colors = require("colors")
+const {errorHandler} = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT || 5000
+
+//connect to db
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false }))
+
+app.use("/api/users", require("./routes/userRoutes"))
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`))
